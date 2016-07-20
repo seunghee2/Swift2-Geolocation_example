3f881fe04e293bae6f73a3eb01bca74d140e2812
seunghee2 <smilagain07@daum.net>
2016-07-20 10:05:50 +0900

    init

---
+++ Where Am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by 이승희 on 7/20/16.
+//  Copyright © 2016 이승희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
